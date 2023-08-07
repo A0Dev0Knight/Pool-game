a6e30d32a19f07aace4da00b8765cfcdc3a61a42
AlexDaniel-nice <112327676+A0Dev0Knight@users.noreply.github.com>
2023-08-07 14:35:41 +0300

    Project changed
    I am now learning shaders instead of making a pool game

---
+++ Pool/Assets/Scripts/ChangeShaderProperty.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeShaderProperty : MonoBehaviour
+{
+    [SerializeField] private Material material;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            material.SetColor("_Color", Color.green);
+        }
+    }
+}
